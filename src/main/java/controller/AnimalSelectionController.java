ade9b3bcedad81c5e784424809df27deac53af3b
Heta <hetahartzell@protonmail.com>
2025-02-08 17:55:38 +0200

    animal selection page

---
+++ src/main/java/controller/AnimalSelectionController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+
+public class AnimalSelectionController extends BaseController {
+
+    //toiminnallisuuksia jeejee
+    @FXML
+    private RadioButton koiratRadio, kissatRadio, hiiretRadio, hevosetRadio;
+
+    @FXML
+    private Button continueButton;
+
+    private String selectedAnimal = "";
+
+    @FXML
+    private void handleHomeClick(MouseEvent event) {
+        switchScene("begin_session");
+    }
+
+    @FXML
+    private void handleProfileClick(MouseEvent event) {
+        switchScene("profile");
+    }
+
+    // kirjaudu ulös
+    @FXML
+    private void handleBackClick(MouseEvent event) {
+//        switchScene("");
+    }
+
+    //edelliselle sivulle
+    @FXML
+    private void handleBack() {
+//        switchScene("");
+    }
+
+    @FXML
+    private void handleContinue() {
+        if (selectedAnimal.isEmpty()) {
+            showAlert(Alert.AlertType.INFORMATION, "valitse", "valitse eläin.");
+        }
+    }
+}
