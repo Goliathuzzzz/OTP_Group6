c182a6876b82136ab6a2b9d256cb4378fea22dbb
Alvaade <ade.aiho2@gmail.com>
2025-02-08 14:02:54 +0200

    implement abstract superclass methods

--- src/main/java/controller/BeginSessionController.java
+++ src/main/java/controller/BeginSessionController.java
@@ -4,37 +4,32 @@ import javafx.scene.control.Alert;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class BeginSessionController {
+public class BeginSessionController extends BaseController {
 
     @FXML
     private ImageView homeIcon, profileIcon, backIcon;
 
     @FXML
     private void handleBeginSessionClick(MouseEvent event) {
-        showAlert("aloitetaan", "siirrytään valintoihin");
+        showAlert(Alert.AlertType.INFORMATION, "aloitetaan", "siirrytään valintoihin");
     }
 
     @FXML
     private void handleHomeClick(MouseEvent event) {
-        showAlert( "kotiin","siirrytään etusivulle");
+        //showAlert( Alert.AlertType.INFORMATION, "kotiin","siirrytään etusivulle");
+        switchScene("begin_session");
     }
 
     @FXML
     private void handleProfileClick(MouseEvent event) {
-        showAlert( "profiiliin","siirrytään profiiliin");
+        //showAlert( Alert.AlertType.INFORMATION, "profiiliin","siirrytään profiiliin");
+        switchScene("profile");
     }
 
     @FXML
     private void handleBackClick(MouseEvent event) {
-        showAlert("takaisin", "siirrytään kirjautumiseen");
-    }
-
-    private void showAlert(String title, String message) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
+        //showAlert(Alert.AlertType.INFORMATION, "takaisin", "siirrytään kirjautumiseen");
+        switchScene("options");
     }
 }
 
