2be2d117a64f3c1e54f0e3c70d07286a89523de1
Goliathuzzzz <mlaakkonen@live.com>
2025-02-16 12:40:43 +0200

    Added categoryFilter to help persist user interests to db

---
+++ src/main/java/util/CategoryFilter.java
@@ -0,0 +1,16 @@
+package util;
+
+import model.categories.Category;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CategoryFilter {
+
+    public static <T extends Enum<T> & Category> List<T> filterEnums(List<Category> categories, Class<T> enumClass) {
+        return categories.stream()
+                .filter(enumClass::isInstance)
+                .map(enumClass::cast)
+                .collect(Collectors.toList());
+    }
+}
