31c60bf35553babbc9a49b98c0e21bf114e9a626
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2025-02-17 18:21:11 +0200

    add AdminProfileController

---
+++ src/main/java/controller/AdminProfileController.java
@@ -0,0 +1,70 @@
+package controller;
+
+import context.GUIContext;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
+import model.User;
+
+public class AdminProfileController extends BaseController {
+
+    private final GUIContext guiContext = GUIContext.getInstance();
+
+    @FXML
+    private Pane bottomNavPane, profileImagePane;
+
+    @FXML
+    private ImageView homeIcon, profileIcon, backIcon, editButton;
+
+    @FXML
+    private Label nameLabel, emailLabel, phoneLabel;
+
+    @FXML
+    private void initialize() {
+        if (guiContext.isUser()) {
+            guiContext.setUserNameProperty(guiContext.getUserName());
+            guiContext.setEmailProperty(guiContext.getUserEmail());
+            guiContext.setPhoneProperty(guiContext.getUserPhoneNumber());
+            nameLabel.textProperty().bind(guiContext.getUserNameProperty());
+            emailLabel.textProperty().bind(guiContext.getEmailProperty());
+            phoneLabel.textProperty().bind(guiContext.getPhoneProperty());
+        }
+    }
+
+    @FXML
+    private void handleHomeClick(MouseEvent event) {
+        //showAlert( Alert.AlertType.INFORMATION, "kotiin","siirrytään etusivulle");
+        switchScene("begin_session");
+    }
+
+    @FXML
+    private void handleProfileClick(MouseEvent event) {
+        switchScene("profile");
+    }
+
+    @FXML
+    private void handleBackClick(MouseEvent event) {
+        switchScene("options");
+    }
+
+    @FXML
+    private void handleUsersClick(MouseEvent event) {
+        switchScene("admin_users");
+    }
+
+    @FXML
+    private void handlePairsClick(MouseEvent event) {
+        switchScene("admin_home");
+    }
+
+    @FXML
+    private void handleEditClick(MouseEvent event) {
+        //showAlert(Alert.AlertType.INFORMATION, "muokkaus", "siirrytään muokkaukseen");
+        switchScene("edit_profile");
+    }
+}
+
