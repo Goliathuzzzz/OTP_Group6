b26bea0fdfb00db709695f618749e83a53bb7e3b
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2025-02-21 16:05:19 +0200

    move to view controllers

--- src/main/java/controller/view_controllers/OptionsController.java
+++ src/main/java/controller/view_controllers/OptionsController.java
@@ -1,10 +1,8 @@
-package controller;
+package controller.view_controllers;
 
+import controller.BaseController;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import util.SceneNames;
