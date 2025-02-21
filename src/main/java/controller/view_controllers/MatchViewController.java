ecd58b2d3eccc528037d7bea3d82c42abc698346
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2025-02-21 15:58:12 +0200

    add MatchViewController.java

---
+++ src/main/java/controller/view_controllers/MatchViewController.java
@@ -0,0 +1,29 @@
+package controller.view_controllers;
+
+import controller.BaseController;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import util.SceneNames;
+
+public class MatchViewController extends BaseController {
+
+    @FXML
+    private void handleHomeClick(MouseEvent event) {
+        switchScene(SceneNames.BEGIN_SESSION);
+    }
+
+    @FXML
+    private void handleProfileClick(MouseEvent event) {
+        switchScene(SceneNames.PROFILE);
+    }
+
+    @FXML
+    private void handleBackClick(MouseEvent event) {
+        switchScene(SceneNames.OPTIONS);
+    }
+
+    @FXML
+    private void handleHelpClick(MouseEvent event) {
+        System.out.println("help");
+    }
+}
