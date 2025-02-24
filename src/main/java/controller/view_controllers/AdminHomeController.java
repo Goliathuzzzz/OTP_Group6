a1dcacb5073a68168c4cdd9c2b8e308640ec9e55
Alvaade <ade.aiho2@gmail.com>
2025-02-24 18:01:06 +0200

    add match removal functionality, replaced pairs with matches, removed switchScene methods

--- src/main/java/controller/view_controllers/AdminHomeController.java
+++ src/main/java/controller/view_controllers/AdminHomeController.java
@@ -6,11 +6,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
-import javafx.util.Pair;
-import model.Participant;
-import util.SceneNames;
+import model.Match;
 
 import java.io.IOException;
 import java.net.URL;
@@ -24,54 +21,46 @@ public class AdminHomeController extends BaseController implements Initializable
     @FXML
     private VBox pairsContainer;
 
-    @FXML
-    private void handleHomeClick(MouseEvent event) {
-        switchScene(SceneNames.BEGIN_SESSION);
-    }
-
-    @FXML
-    private void handleProfileClick(MouseEvent event) {
-        switchScene(SceneNames.PROFILE);
-    }
-
-    @FXML
-    private void handleBackClick(MouseEvent event) {
-        switchScene(SceneNames.OPTIONS);
-    }
-
-    @FXML
-    private void handlePreviousClick(MouseEvent mouseEvent) {
-        switchScene(SceneNames.PROFILE);
-    }
-
-    @FXML
-    private void handleHelpClick(MouseEvent mouseEvent) {
-        System.out.println("help");
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // fetch pairs from database (currently dummy data)
-        List<Pair<Participant, Participant>> pairs = fetchPairsFromDatabase();
+        List<Match> matches = matchController.displayAllMatches();
+        populateMatchList(matches);
+    }
 
-        for (Pair<Participant, Participant> pair : pairs) {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/pair_item.fxml"));
-                Node pairNode = loader.load();
-
-                // get the controller for the pair item and set the names
-                PairItemController pairController = loader.getController();
-                pairController.setParticipants(pair.getKey(), pair.getValue());
-
-                // add the loaded pair item to the container
-                pairsContainer.getChildren().add(pairNode);
-            } catch (IOException e) {
-                System.err.println("Error loading pair item: " + e.getMessage());
+    private void populateMatchList(List<Match> matches) {
+        for (Match match : matches) {
+            Node matchNode = createMatchItemNode(match);
+            if (matchNode != null) {
+                pairsContainer.getChildren().add(matchNode);
             }
         }
     }
 
-    private List<Pair<Participant, Participant>> fetchPairsFromDatabase() {
-        return matchController.fetchPairsFromDatabase();
+    private Node createMatchItemNode(Match match) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/pair_item.fxml"));
+            Node matchNode = loader.load();
+            PairItemController controller = loader.getController();
+            controller.setMatch(match, this::deleteMatch);
+            matchNode.setUserData(controller);
+            return matchNode;
+        } catch (IOException e) {
+            System.err.println("ERROR: Could not load pair item: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private void deleteMatch(Match match) {
+        matchController.deleteMatch(match);
+        removeMatchFromList(match);
+    }
+
+    private void removeMatchFromList(Match match) {
+        pairsContainer.getChildren().stream()
+                .filter(node -> {
+                    PairItemController controller = (PairItemController) node.getUserData();
+                    return controller != null && controller.getMatch().equals(match);
+                })
+                .findFirst().ifPresent(nodeToRemove -> pairsContainer.getChildren().remove(nodeToRemove));
     }
 }
