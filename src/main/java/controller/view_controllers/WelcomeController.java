3a1a8017f9b02395218cfe25658f530d8b47cfc7
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2025-02-24 14:21:00 +0200

    add pulsating effect

--- src/main/java/controller/view_controllers/WelcomeController.java
+++ src/main/java/controller/view_controllers/WelcomeController.java
@@ -1,14 +1,29 @@
 package controller.view_controllers;
 
 import controller.BaseController;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 import util.SceneNames;
 
 public class WelcomeController extends BaseController {
 
+    public ImageView logo;
+
     @FXML
     public void initialize() {
         System.out.println("tervetuloa!");
+        startPulsatingLogo();
+    }
+
+    private void startPulsatingLogo() {
+        ScaleTransition pulsate = new ScaleTransition(Duration.seconds(1), logo);
+        pulsate.setByX(0.1);
+        pulsate.setByY(0.1);
+        pulsate.setCycleCount(ScaleTransition.INDEFINITE);
+        pulsate.setAutoReverse(true);
+        pulsate.play();
     }
 
     @FXML
