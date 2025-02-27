20697ed4c8d7d4860545a0ecf03fcfaa5bffb0f2
Alvaade <ade.aiho2@gmail.com>
2025-02-27 14:36:55 +0200

    create MatchUtils for AfterMatchController to use

---
+++ src/main/java/util/MatchUtils.java
@@ -0,0 +1,22 @@
+package util;
+
+import model.categories.Category;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MatchUtils {
+    public static List<Category> findCommonInterests(List<Category> list1, List<Category> list2) {
+        System.out.println("DEBUG: Finding common interests between " + list1 + " and " + list2);
+        List<Category> commonInterests = new ArrayList<>(list1);
+        commonInterests.retainAll(list2);
+        return commonInterests;
+    }
+
+    public static String formatInterests(List<Category> interests) {
+        return interests.stream()
+                .map(Category::toString)
+                .map(s -> s.toLowerCase().replace("_", " "))
+                .collect(Collectors.joining(", "));
+    }
+}
