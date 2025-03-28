93f20167dd69cae501a1c0b0d1f1aa6f12deaa1e
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2025-03-28 15:09:01 +0200

    import LocaleManager, add localized stage title

--- src/main/java/controller/view_controllers/HelpController.java
+++ src/main/java/controller/view_controllers/HelpController.java
@@ -1,10 +1,15 @@
 package controller.view_controllers;
 
+import context.LocaleManager;
 import controller.BaseController;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
 
 public class HelpController extends BaseController {
 
@@ -18,8 +23,22 @@ public class HelpController extends BaseController {
     private VBox vBox;
 
     @FXML
+    private AnchorPane helpPane;
+
+    private final LocaleManager localeManager = LocaleManager.getInstance();
+    private final ResourceBundle bundle = localeManager.getBundle();
+
+    @FXML
     public void initialize() {
         vBox.setAlignment(javafx.geometry.Pos.CENTER);
         vBox.setPadding(new javafx.geometry.Insets(0, 0, 50, 0));
+        Platform.runLater(() -> {
+            Stage stage = (Stage) helpPane.getScene().getWindow();
+            if (stage != null) {
+                stage.setTitle(bundle.getString("help"));
+            } else {
+                System.err.println("Stage is null in HelpController initialize()");
+            }
+        });
     }
 }
