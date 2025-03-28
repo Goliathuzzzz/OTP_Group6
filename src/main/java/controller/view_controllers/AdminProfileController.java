e3a8515b8c5038ff11b87ba8960221d0da82557c
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2025-03-28 15:04:26 +0200

    import LocaleManager, add localized stage title

--- src/main/java/controller/view_controllers/AdminProfileController.java
+++ src/main/java/controller/view_controllers/AdminProfileController.java
@@ -1,13 +1,19 @@
 package controller.view_controllers;
 
 import context.GUIContext;
+import context.LocaleManager;
 import controller.BaseController;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import util.SceneNames;
+
+import java.util.ResourceBundle;
 
 import static util.ProfilePictureUtil.getProfilePictureView;
 
@@ -23,6 +29,12 @@ public class AdminProfileController extends BaseController {
 
     @FXML
     private Label nameLabel, emailLabel, phoneLabel;
+
+    @FXML
+    private AnchorPane adminProfilePane;
+
+    private final LocaleManager localeManager = LocaleManager.getInstance();
+    private final ResourceBundle bundle = localeManager.getBundle();
 
     // default constructor for FXML loader
     public AdminProfileController() {
@@ -47,6 +59,14 @@ public class AdminProfileController extends BaseController {
             ImageView profileImageView = getProfilePictureView(guiContext.getId(), 200, 200);
             profileImagePane.getChildren().add(profileImageView);
         }
+        Platform.runLater(() -> {
+            Stage stage = (Stage) adminProfilePane.getScene().getWindow();
+            if (stage != null) {
+                stage.setTitle(bundle.getString("admin_profile"));
+            } else {
+                System.out.println("Stage is null in AdminProfileController initialize()");
+            }
+        });
     }
 
     @FXML
