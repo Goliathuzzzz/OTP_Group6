910e3a497fea95e49ac9dace9cc02c4ca56df348
Goliathuzzzz <mlaakkonen@live.com>
2025-04-08 14:19:38 +0300

    Refactored Matcher

--- src/main/java/model/Matcher.java
+++ src/main/java/model/Matcher.java
@@ -9,7 +9,8 @@ import model.categories.Category;
 
 public class Matcher {
     private final Session session;
-    // Double arvo on yhteensopivuus-prosentti. Periaatteessa voi olla useampi paras match, niin tallennetaan hashmappiin
+    // Double arvo on yhteensopivuus-prosentti.
+    // Periaatteessa voi olla useampi paras match, niin tallennetaan hashmappiin
     private final HashMap<User, Double> topMatches;
     private UserController userController;
 
@@ -21,12 +22,6 @@ public class Matcher {
 
     // Kutsu kun käyttäjä on vahvistanut valintansa GUIssa
     public void matchParticipant() {
-        double compatibility;
-        double currentHighestCompatibility = 0;
-        double maxPotential;
-        double increment;
-        List<Category> pMatchInterests;
-
         // check if session participant is null
         Participant toMatch = session.getParticipant();
         if (toMatch == null) {
@@ -48,57 +43,49 @@ public class Matcher {
             return;
         }
 
-        System.out.println("DEBUG: Matching for participant " + toMatch.getDisplayName("en"));
-        System.out.println("DEBUG: Participant interests: " + participantInterests);
+        double compatibility;
+        double currentHighestCompatibility = 0;
+        double maxPotential;
+        double increment;
+        List<Category> potentialMatchInterests;
 
         for (User u : potentialMatches) {
             if (u.equals(toMatch)) {
                 continue; // exclude participant from matches
 
             }
-            System.out.println("DEBUG: Checking user: " + u.getId());
 
-            pMatchInterests = u.getInterests();
-            if (pMatchInterests.isEmpty()) {
+            potentialMatchInterests = u.getInterests();
+            if (potentialMatchInterests.isEmpty()) {
                 continue; // skip if user has no interests
 
             }
             compatibility = 0;
             maxPotential = 100;
-            increment = (double) 100 / pMatchInterests.size();
+            increment = (double) 100 / potentialMatchInterests.size();
 
             for (Category interest : session.getParticipantInterests()) {
-                if (pMatchInterests.contains(interest)) {
+                if (potentialMatchInterests.contains(interest)) {
                     compatibility += increment;
-                    System.out.println(
-                            "DEBUG: Match found for interest: " + interest + " -> compatibility: " +
-                                    compatibility);
                 } else {
                     maxPotential -= increment;
-                    System.out.println("DEBUG: No match found for interest: " + interest +
-                            " -> max potential: " + maxPotential);
                 }
                 if (maxPotential < currentHighestCompatibility) {
-                    System.out.println("DEBUG: Skipping " + u.getId() + " due to low potential.");
                     break;
                 }
             }
             compatibility = roundToTwoDecimalPlaces(compatibility);
-            System.out.println("DEBUG: Compatibility for " + u.getId() + ": " + compatibility);
 
             if (compatibility > currentHighestCompatibility) {
                 topMatches.clear();
                 topMatches.put(u, compatibility);
                 currentHighestCompatibility = compatibility;
-                System.out.println("DEBUG: New highest compatibility: " + compatibility);
             } else if (compatibility == currentHighestCompatibility) {
                 topMatches.put(u, compatibility);
-                System.out.println("DEBUG: Added " + u.getId() + " to top matches.");
             }
         }
         if (currentHighestCompatibility == 0) {
             topMatches.clear();
-            System.out.println("DEBUG: No matches found.");
         }
         if (topMatches.isEmpty()) {
             System.err.println("ERROR: No top matches found in Matcher matchParticipant()");
