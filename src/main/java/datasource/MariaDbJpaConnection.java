7eac4632504e20c53cc9ae010ea1a08b5c74e9d2
Alvaade <ade.aiho2@gmail.com>
2025-04-08 12:34:51 +0300

    refactor lazy initialization to thread safe one

--- src/main/java/datasource/MariaDbJpaConnection.java
+++ src/main/java/datasource/MariaDbJpaConnection.java
@@ -4,16 +4,20 @@ import jakarta.persistence.*;
 
 public class MariaDbJpaConnection {
 
-    private static EntityManagerFactory emf;
-    private static EntityManager em;
+    private static volatile EntityManagerFactory emf;
+    private static volatile EntityManager em;
 
     public static EntityManager getInstance() {
 
         if (em==null) {
-            if (emf==null) {
-                emf = Persistence.createEntityManagerFactory("TatskatytotMariaDbUnit");
+            synchronized (MariaDbJpaConnection.class) {
+                if (em == null) {
+                    if (emf == null) {
+                        emf = Persistence.createEntityManagerFactory("TatskatytotMariaDbUnit");
+                    }
+                    em = emf.createEntityManager();
+                }
             }
-            em = emf.createEntityManager();
         }
         return em;
     }
